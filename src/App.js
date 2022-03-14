a1ac56057c6efda2bf4ec43a6771662b3826105f
Fabi Arev <fabi.arev@gmail.com>
2022-03-13 22:03:40 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+       <p>
+         Edit <code>src/App.js</code> and save to reload.
+         
         </p>
+        <p> Tienda virtual en React</p>
         <a
           className="App-link"
           href="https://reactjs.org"
