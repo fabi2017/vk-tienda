8bb9b8336d71a201120ffc3a1176c1c31a337540
Fabi Arev <fabi.arev@gmail.com>
2022-03-16 15:40:30 -0300

    segundo desafio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 
 import './App.css';
+import NavBar from './componentes/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <p>
-         Edit <code>src/App.js</code> and save to reload.
-         
-        </p>
-        <p> Tienda virtual en React</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <NavBar />
     </div>
   );
 }
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import Link from '@mui/material/Link';
+import { lightBlue } from '@mui/material/colors';
+
+const color = lightBlue[100];
+
+
+
+function NavBar() {
+  return (   
+      <header className="App-header">
+          <div>
+              <img className="imgLogo" src = "logo2.png"/>
+
+          </div>
+
+          <ul className='navBar'>          
+            <Link href="#" underline="hover" sx={{ p: 2 , fontSize: 'h6.fontSize', fontWeight: 'medium',color }} >{'Inicio'}</Link>
+            <Link href="#" underline="hover" sx={{ p: 2 , fontSize: 'h6.fontSize', fontWeight: 'medium', color}} >{'Productos'}</Link>
+            <Link href="#" underline="hover" sx={{ p: 2 , fontSize: 'h6.fontSize', fontWeight: 'medium', color }} >{'Nosotros'}</Link>
+            <Link href="#" underline="hover" sx={{ p: 2 , fontSize: 'h6.fontSize', fontWeight: 'medium',color }}  >{'Contacto'}</Link>
+          </ul>
+      
+      </header>
+      
+    );
+}
+
+export default NavBar;
